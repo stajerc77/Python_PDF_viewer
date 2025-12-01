8f2cc4f0e8bb71a5f512aacc7c7dd859949d426d
stajerc77 <mihak.dev@gmail.com>
2025-12-01 11:27:04 +0100

    Add ner extractor script

---
+++ ner_extractor.py
@@ -0,0 +1,10 @@
+from gliner2 import GLiNER2
+
+
+# Load model once, use everywhere
+ner_extractor = GLiNER2.from_pretrained("fastino/gliner2-base-v1")
+
+# extract named entities
+def extract_entities(text: str):
+    entities = ner_extractor.extract_entities(text, ["company", "person", "product", "location", "date"])
+    return entities
