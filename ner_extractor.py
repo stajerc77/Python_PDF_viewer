ed0ff551da823fbdc6bbdebc7c54734d3fdb1ff1
stajerc77 <mihak.dev@gmail.com>
2025-12-01 11:28:39 +0100

    Add comment

--- ner_extractor.py
+++ ner_extractor.py
@@ -6,5 +6,6 @@ ner_extractor = GLiNER2.from_pretrained("fastino/gliner2-base-v1")
 
 # extract named entities
 def extract_entities(text: str):
+    # here, you can adjust the custom list of extracted entities to your desires
     entities = ner_extractor.extract_entities(text, ["company", "person", "product", "location", "date"])
     return entities
